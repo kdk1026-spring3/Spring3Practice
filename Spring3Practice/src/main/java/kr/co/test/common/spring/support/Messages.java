9f765c3941752af000748ab09876ed05cf766e3d
kdk1026 <kdk1026@gmail.com>
2024-10-17 20:31:00 +0900

    kdk1026-spring3/Spring3Practice 에서 가져옴

--- Spring3Practice/src/main/java/kr/co/test/common/spring/support/Messages.java
+++
@@ -1,7 +0,0 @@
-package kr.co.test.common.spring.support;
-
-public interface Messages {
-
-	public String getMessage(String code, String lang);
-	
-}
--- Spring3Practice/src/main/java/kr/co/test/page/login/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package kr.co.test.page.login.service;
-
-import common.spring.resolver.ParamCollector;
-
-public interface LoginService {
-
-	public void updateLastLoginDt(ParamCollector paramCollector);
-	
-}
